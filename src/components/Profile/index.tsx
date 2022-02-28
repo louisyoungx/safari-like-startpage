d511120d2e8fff47456584827870578de354b5cf
louisyoungx <1462648167@qq.com>
2022-02-28 18:28:18 +0800

    feat: bookmark & group wrap box

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './index.css'
+import Image from '../Image'
+
+interface ProfileProps {
+    name: string
+    url: string
+    img: string
+    tags: string | null
+    views: string | null
+    discription: string
+    type: 'bookmark' | 'group'
+    handleClick: Function
+}
+
+const Profile: React.FC<ProfileProps> = (props: ProfileProps) => {
+    if (props.type === 'bookmark') {
+        return (
+            <a className='profile-bookmark' href={props.url}>
+                <figure className='profile'>
+                    <div className='profile-img-wrap'>
+                        <Image {...props} />
+                    </div>
+                    <figcaption className='profile-title-wrap'>
+                        <p className='profile-title'>{props.name}</p>
+                    </figcaption>
+                </figure>
+            </a>
+        )
+    } else {
+        return (
+            <div className='profile-folder' onClick={() => props.handleClick(props.url)}>
+                <figure className='profile'>
+                    <div className='profile-img-wrap'>
+                        <Image {...props} />
+                    </div>
+                    <figcaption className='profile-title-wrap'>
+                        <p className='profile-title'>{props.name}</p>
+                    </figcaption>
+                </figure>
+            </div>
+        )
+    }
+}
+
+export default Profile
