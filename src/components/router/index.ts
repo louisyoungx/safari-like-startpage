c8c45b92064a94c7a9c14c7a53ad1142d3bd56d0
louisyoungx <1462648167@qq.com>
2022-02-28 18:29:19 +0800

    feat: basic router functions

---
+++ src/components/router/index.ts
@@ -0,0 +1,17 @@
+import { mainView } from "../../config/bookmark"
+
+class Router {
+    private main: string
+    constructor(main: string) {
+        this.main = main
+    }
+    getPath() {
+        return window.location.pathname.replace('/', '')
+    }
+    changePath(path: string) {
+        // eslint-disable-next-line no-restricted-globals
+        history.pushState(null, '', path)
+    }
+}
+
+export default new Router(mainView)
