f512a0323566d9712537b390e6f9fbf68ede1078
louisyoungx <1462648167@qq.com>
2022-03-03 18:02:04 +0800

    perf: get img error handle

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,19 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Bookmark } from '../config/bookmark'
 
 const Image: React.FC<Bookmark> = (props: Bookmark) => {
-    const [src, useSrc] = useState(props.img)
+    let placeholder: string = '/notFound.svg'
+    const [src, useSrc] = useState(placeholder)
+    const [errIndex, useErrIndex] = useState(0)
 
-    const HandleImageError = (bookmark: Bookmark) => {
-        let img: string = '/notFound.svg'
-        useSrc(img)
+    const InitImage = () => {
+        useSrc(props.img)
     }
+
+    const HandleImageError = () => {
+        const proxyList: string[] = [
+            `https://t1.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${props.url}&size=64`,
+            `http://logo.clearbit.com/${props.url}?size=80&greyscale=true`,
+            '/notFound.svg',
+        ]
+        useSrc(proxyList[errIndex])
+        useErrIndex(errIndex + 1)
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            InitImage()
+        }, 0)
+    }, [])
 
     return (
         <img 
             className='profile-img' 
             src={src} alt={props.name}
-            onError={() => HandleImageError(props)}
+            onError={() => HandleImageError()}
         />
     )
 }
