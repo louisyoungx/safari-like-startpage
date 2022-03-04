f61f4ce79b59bb4d86c0f02d30ea4d2213612f43
louisyoungx <1462648167@qq.com>
2022-03-04 20:14:20 +0800

    feat: wallpaper config

---
+++ src/config/wallpaper.ts
@@ -0,0 +1,16 @@
+const wallpaper = [
+    {
+        url: '/img/wallpaper/Safari_Background_Pink.jpg',
+    },
+    {
+        url: '/img/wallpaper/Safari_Background_Orange.jpg',
+    },
+    {
+        url: '/img/wallpaper/Safari_Background_Blue.jpg',
+    },
+    {
+        url: '/img/wallpaper/Aeolian3.jpg',
+    },
+]
+
+export default wallpaper
